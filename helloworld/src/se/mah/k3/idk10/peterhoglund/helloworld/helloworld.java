35123754502dc3dc1e688f605360ed940daa7441
Peter <m10k0944@student.mah.se>
2011-04-01 14:51:19 +0200

    Signed-off-by: Peter <m10k0944@student.mah.se>

---
+++ helloworld/src/se/mah/k3/idk10/peterhoglund/helloworld/helloworld.java
@@ -0,0 +1,13 @@
+package se.mah.k3.idk10.peterhoglund.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class helloworld extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
